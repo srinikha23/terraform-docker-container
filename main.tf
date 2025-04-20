2f7cc2d72fffe9e0bd6cfe90c9ca21b4f0ad90aa
srinikha23 <srinikha2399@gmail.com>
2025-04-20 08:37:57 +0000

    Completed Terraform Docker container task

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx_container"
+  image = docker_image.nginx.name
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
